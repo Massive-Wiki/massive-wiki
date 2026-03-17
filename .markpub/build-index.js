c651d56f616b120ab3a0ab48c25e652da2be5f74
William L. Anderson <band@acm.org>
2026-03-17 16:57:09 -0500

    update the infrastructure stuff

---
+++ .markpub/build-index.js
@@ -0,0 +1,28 @@
+// from https://lunrjs.com/guides/index_prebuilding.html
+var lunr = require('lunr'),
+    stdin = process.stdin,
+    stdout = process.stdout,
+    buffer = []
+
+stdin.resume()
+stdin.setEncoding('utf8')
+
+stdin.on('data', function (data) {
+  buffer.push(data)
+})
+
+stdin.on('end', function () {
+  var documents = JSON.parse(buffer.join(''))
+
+  var idx = lunr(function () {
+    this.ref('link')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+      this.add(doc)
+    }, this)
+  })
+
+  stdout.write(JSON.stringify(idx))
+})
